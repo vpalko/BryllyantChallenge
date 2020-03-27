df25a5101d3260a16fcb3bcc1332ecee78b4eb54
Vladimir Palko <palko@us.ibm.com>
2020-03-27 11:59:45 -0400

    postgres script update

--- postgresDocker/init-db.sql
+++ postgresDocker/init-db.sql
@@ -1,5 +1,3 @@
--- CREATE USER bryllyantuser WITH PASSWORD '123';
 CREATE DATABASE demo;
 \connect demo;
--- GRANT ALL PRIVILEGES ON DATABASE demo TO bryllyantuser;
 CREATE SCHEMA IF NOT EXISTS bryllyant;
